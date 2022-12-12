6be1b7d4a8081b445b3bc24a52816f6dd3ae8101
kaxuu <107888279+kaxuu@users.noreply.github.com>
2022-12-12 16:56:00 +0100

    polski język

--- server/app.py
+++ server/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/name')
 def name():  # put application's code here
-    return {"name": "Tutaj będzie można dodać adnotacje do zdjęć."}
+    return json.dumps({"name": "Tutaj będzie można dodać adnotacje do zdjęć."}, ensure_ascii=False)
 
 
 if __name__ == '__main__':
