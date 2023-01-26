cbda4ec2f3f591a1b5959165c2c6cb92d6d72a1f
kaxuu <107888279+kaxuu@users.noreply.github.com>
2023-01-26 05:20:49 +0100

    pobieranie zipa z resizowanymi obrazkami

--- client/src/views/Export.vue
+++ client/src/views/Export.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="wrap">
-    <Button label="Eksportuj wszystkie" @click="loadAnnotatedImages(); exportAll()" class="tools"/>
-    <Button v-if="!img_visible" label="Wybierz zdjęcia do eksportu" @click="loadAnnotatedImages(); img_visible=true;" class="tools"/>
-    <Button v-if="img_visible" label="Eksportuj wybrane" @click=" exportNotAll()" class="tools" id="exp"/>
+    <div v-if="!img_visible">
+      <p>{{"Podaj nazwę projektu:"}}</p>
+        <input type="text" id="project_name" defaultValue="Projekt"/>
+    </div>
+    <Button v-if="!img_visible" label="Dalej" @click="loadAnnotatedImages(); img_visible=true;" class="tools"/>
+    <Button v-if="img_visible" label="Eksportuj wszystkie" @click="loadAnnotatedImages(); exportAll();" class="tools"/>
+    <Button v-if="img_visible" label="Eksportuj wybrane" @click=" loadAnnotatedImages();exportNotAll()" class="tools" id="exp"/>
   </div>
   <div class="imggrid">
     <ul>
@@ -14,8 +18,34 @@
 </template>
 
 <script>
+
 import {app} from "@/main";
 import axios from "axios";
+import resizebase64 from "resize-base64";
+import JSZip from "jszip";
+import { saveAs } from 'file-saver';
+
+function b64toBlob(dataURI) {
+
+    var byteString = atob(dataURI.split(',')[1]);
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ab], { type: 'image/jpeg' });
+}
+
+const saveZip = (filename, urls) => {
+    let zip = new JSZip();
+    let folder = zip.folder(filename);
+    for(let i=0;i<urls.length;i++)
+    {
+      folder.file(`image${i}.jpg`, urls[i]);
+    }
+    folder.generateAsync({ type: "blob" }).then(content => saveAs(content, filename));
+};
 
 export default {
   name: "Export",
@@ -26,18 +56,19 @@ export default {
       annotated:[],
       displayed_annotated_images:[],
       to_download:[],
+      p_name:"project",
     }
   },
   methods: {
     async loadAnnotatedImages() {
-          const dict = {
+      // this.p_name=document.getElementById('project_name').value;
+      const dict = {
             "login": app.config.globalProperties.$login.value,
             "password": app.config.globalProperties.$password.value,
           };
           const annotated = [];
           const annotated_ind = [];                                                                                                                                                                                                                                                                                                                                                                                                                                                                             0
-          await axios.post("http://localhost:5000/get_annotated_images_no_rects",
-              {params: JSON.stringify(dict)}).then(function (response) {
+          await axios.post("http://localhost:5000/get_annotated_images_no_rects", {params: JSON.stringify(dict)}).then(function (response) {
             let data = response.data["images"];
             for(let i = 0; i < data.length; i++)
             {
@@ -53,22 +84,25 @@ export default {
     async exportNotAll(){
       // tu jeszcze nic nie ma
     },
-    async exportAll() {
-      // tu beda zdjecia resizowane
-      const to_download=[];
-      for(let i=0;i<this.annotated.length;i++)
-      {
-        var resizebase64 = require('resize-base64');
-        var  img = resizebase64(this.annotated[i], 200, 150);
-        to_download.push(img);
-      }
-      this.to_download=to_download;
 
-      // testowy download jednego zdj
-      var a = document.createElement("a"); //Create <a>
-      a.href = this.to_download[0]; //Image Base64 Goes here
-      a.download = "Image.png"; //File name Here
-      a.click(); //Downloaded file
+    async exportAll() {
+      await this.loadAnnotatedImages();
+      // this.p_name=document.getElementById('project_name').value;
+      if(this.annotated)
+      {
+        console.log(this.annotated);
+        const to_download=[];
+        for(let i=0;i<this.annotated.length;i++)
+        {
+          var resizebase64 = require('resize-base64');
+          var  img = resizebase64(this.annotated[i], 666, 500);
+          var blob = b64toBlob(img)
+          to_download.push(blob);
+        }
+        this.to_download=to_download;
+        saveZip(this.p_name,this.to_download);
+      }
+
     },
 }}
 </script>
@@ -92,5 +126,8 @@ export default {
   cursor: pointer;
 }
 
+#project_name{
+  margin-bottom: 30px;
+}
 </style>
 
